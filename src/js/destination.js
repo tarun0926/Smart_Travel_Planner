120cfc0672d8e11da77a1fd8cb8c88e962bf84a7
tarun0926 <kotiyatarun639@gmail.com>
2025-12-19 10:06:39 +0530

    day3 done

--- src/js/destination.js
+++ src/js/destination.js
@@ -5,8 +5,46 @@ const city = params.get("city")
 
 const cityName = document.querySelector("#cityName");
 
+let GEODB_API_KEY = "0195a03772mshbea4f740b029fc9p13095bjsn8ee1b9144628"; //starting point
+let GEODB_HOST = "wft-geo-db.p.rapidapi.com"; //end point
+
 if(city){
     cityName.textContent = city;
+
+    async function fetchCityDetails(cityName) {
+        try{
+            let res = await fetch(`https://${GEODB_HOST}/v1/geo/cities?namePrefix=${city}&limit=1`,
+                {
+                    method: "GET",
+                    headers : {
+                        "X-RapidAPI-Key": GEODB_API_KEY,
+                        "X-RapidAPI-Host":  GEODB_HOST,
+                    },
+                }
+            );
+            console.log(res);
+
+            if(!res.ok){
+                console.log("API Response Not Fetched..");
+            }
+
+            let data = await res.json();
+            console.log(data);
+
+            if(!data.data || data.data.length === 0){
+                console.log("City Not Found");
+            }
+            let cityData = data.data[0];
+            console.log(cityData);
+            
+            
+            //Display City Info
+        }catch(error){
+            console.log(error); 
+        }
+    }
+
+    fetchCityDetails(city);
 }else{
     cityName.textContent = "Unknown City"
 }
